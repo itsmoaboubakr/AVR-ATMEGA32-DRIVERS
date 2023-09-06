ae0febf2cf77c6aba3b710f49133d7c29fab8890
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-09-07 02:48:55 +0300

    Create MGIE_interface.h

---
+++ 2-MCAL/MGIE/MGIE_interface.h
@@ -0,0 +1,15 @@
+/************************************/
+/*   Author:    mBravo18            */
+/*   SWC:       GLOBAL INTERRUPT    */
+/*   Layer:     MCAL                */
+/*   Date:      September 07, 2023  */
+/*   Last Edit: N/A                 */
+/************************************/
+
+#ifndef _MGIE_INTERFACE_H_
+#define _MGIE_INTERFACE_H_
+
+void MGIE_voidDisableGI(void);
+void MGIE_voidEnableGI(void);
+
+#endif
