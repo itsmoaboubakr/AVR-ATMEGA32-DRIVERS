bb2e0fad797bd5832c0fcdfd39d7bf292ef28f92
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-09-07 02:54:53 +0300

    Create MGIE_private.h

---
+++ 2-MCAL/MGIE/MGIE_private.h
@@ -0,0 +1,13 @@
+/************************************/
+/*   Author:    mBravo18            */
+/*   SWC:       GLOBAL INTERRUPT    */
+/*   Layer:     MCAL                */
+/************************************/
+
+#ifndef _MGIE_PRIVATE_H_
+#define _MGIE_PRIVATE_H_
+
+#define SREG	*((volatile u8*)0x5F)
+#define IBBIT	7
+
+#endif
