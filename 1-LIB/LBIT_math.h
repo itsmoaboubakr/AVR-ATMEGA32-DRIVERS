37245762eb0302fbfedf1434f76aff68ba442725
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-09-13 21:09:43 +0300

    Update LBIT_math.h

--- 1-LIB/LBIT_math.h
+++ 1-LIB/LBIT_math.h
@@ -7,9 +7,9 @@
 #ifndef _LBIT_MATH_H_
 #define _LBIT_MATH_H_
 
-#define 	SET_BIT(reg,bit)     reg |= (1 << bit)
-#define 	CLEAR_BIT(reg,bit)   reg &= ~(1 << bit)
-#define 	TOGGLE_BIT(reg,bit)  reg ^= (1 << bit)
-#define 	GET_BIT(reg,bit)     ((reg >> bit) & 1)
+#define 	SET_BIT(reg,bit)  reg |=  (1 << bit)
+#define 	CLR_BIT(reg,bit)  reg &= ~(1 << bit)
+#define 	TOG_BIT(reg,bit)  reg ^=  (1 << bit)
+#define 	GET_BIT(reg,bit)  ((reg >> bit) & 1)
 
 #endif
