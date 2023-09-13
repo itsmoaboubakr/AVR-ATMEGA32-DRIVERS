2a9fa8ed8f33a6c585b828be47535699dd1a60fb
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-09-13 21:11:10 +0300

    Update LSTD_types.h

--- 1-LIB/LSTD_types.h
+++ 1-LIB/LSTD_types.h
@@ -7,10 +7,10 @@
 #ifndef _LSTD_TYPES_H_
 #define _LSTD_TYPES_H_
 
-typedef unsigned char       u8;
+typedef unsigned char       u8 ;
 typedef unsigned short int  u16;
 typedef unsigned long int   u32;
-typedef signed char         s8;
+typedef signed char         s8 ;
 typedef signed short int    s16;
 typedef signed long int     s32;
 typedef float               f32;
